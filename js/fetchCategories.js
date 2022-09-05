2d9ab346ccf9706cb31eeb4e20a6cf937d394e51
Hector <gt12930@GMAIL.com>
2022-09-04 23:26:03 -0500

    commit inicial

---
+++ js/fetchCategories.js
@@ -0,0 +1,8 @@
+export const fetchCategories = async () => {
+    const response = await fetch("https://bsale2.herokuapp.com/api/v1/categories");
+    const data = await response.json();
+    if(data){
+      renderCategories(data)
+    }
+};
+  
